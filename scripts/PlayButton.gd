03f21e0757611f84c9621865e1ed019a90725bc3
kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-09-20 17:17:25 +0200

    style: organise files

--- scripts/PlayButton.gd
+++ scripts/PlayButton.gd
@@ -5,7 +5,7 @@ func _ready():
 	pass
 
 func _pressed() -> void:
-	get_tree().change_scene_to_file("res://survivors_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/survivors_game.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
