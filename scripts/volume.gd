6c6805339be224d1e5d20804d41877b655ce1afb
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-07 22:01:05 +0200

    feat: add new weapon design

--- scripts/volume.gd
+++ scripts/volume.gd
@@ -13,10 +13,6 @@ func _on_master_value_changed(value: float) -> void:
 	var bus_index = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 	AudioServer.set_bus_mute(bus_index, value < 0.01)
-
-
-
-
 func _on_music_value_changed(value: float) -> void:
 	var bus_index = AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
@@ -31,7 +27,7 @@ func _on_sfx_value_changed(value: float) -> void:
 
 
 func _on_bullets_value_changed(value: float) -> void:
-	var bus_index = AudioServer.get_bus_index("shot")
+	var bus_index = AudioServer.get_bus_index("Bullets")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 	AudioServer.set_bus_mute(bus_index, value < 0.01)
 
