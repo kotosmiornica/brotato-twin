00001c3a71ed44f5415fd2420df081d34a971bd2
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-09 21:48:23 +0200

    feat: add purple shade, fix a few things

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -2,29 +2,37 @@ extends Area2D
 
 var player: Node = null
 @export var shooting_point: Node2D
+@onready var timer: Timer = $Timer  # reference to your Timer node
 
 const BULLET = preload("res://scenes/bullet.tscn")
 var shoot_sound = preload("res://SOUNDS/sounds/simpleshot.mp3")
 
 func _physics_process(_delta: float) -> void:
 	var enemies_in_range = get_overlapping_bodies()
+
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
 
-func shoot():
+		# ✅ Start shooting timer if not already running
+		if not timer.is_stopped():
+			return
+		timer.start()
+	else:
+		# ✅ Stop shooting when no enemies are nearby
+		timer.stop()
 
+func shoot() -> void:
 	var new_bullet = BULLET.instantiate()
-	new_bullet.global_position = %ShootingPoint.global_position
-	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	new_bullet.global_position = shooting_point.global_position
+	new_bullet.global_rotation = shooting_point.global_rotation
 	get_tree().current_scene.add_child(new_bullet)
-
 
 	_play_detached_sound(shoot_sound, global_position)
 
 func _on_timer_timeout() -> void:
 	shoot()
-	
+
 func _play_detached_sound(sound_stream: AudioStream, sound_position: Vector2):
 	var sound = AudioStreamPlayer2D.new()
 	sound.stream = sound_stream
@@ -34,7 +42,8 @@ func _play_detached_sound(sound_stream: AudioStream, sound_position: Vector2):
 	sound.play()
 
 	var duration = sound.stream.get_length()
-	if duration <= 0.0: duration = 2.0
+	if duration <= 0.0:
+		duration = 2.0
 
 	var timer = Timer.new()
 	timer.wait_time = duration
