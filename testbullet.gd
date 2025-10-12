53e91c8700dc2a900c8f066bbef57ac77cd203a3
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-13 01:02:48 +0200

    feat: implement the gun into the game

--- testbullet.gd
+++ testbullet.gd
@@ -1,10 +1,19 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var speed = 800.0
-var direction = Vector2.RIGHT
+@export var speed: float = 1000.0
+@export var damage: int = 10
+var direction: Vector2 = Vector2.RIGHT
 
-func _physics_process(delta):
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+
+func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func _on_body_entered(body: Node) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
