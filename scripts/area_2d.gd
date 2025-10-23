eb0aabb73e9b6b0a360a994d919b03cc959c0dbc
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-24 00:53:58 +0200

    feat: make xp dissappear after some time

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -4,8 +4,14 @@ signal collected
 
 func _ready():
 	connect("body_entered", _on_body_entered)
+	_start_despawn_timer()
 
 func _on_body_entered(body):
 	if body.name == "Brotat":
 		collected.emit()
+		queue_free()
+
+func _start_despawn_timer():
+	await get_tree().create_timer(10.0).timeout
+	if is_inside_tree():
 		queue_free()
