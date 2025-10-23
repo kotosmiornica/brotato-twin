b9a4bb67b1cbaeac53689083991ec2e16a467ba2
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-23 19:48:43 +0200

    feat: delete clicking the cat :(

--- scripts/MakeYourPlayer.gd
+++ scripts/MakeYourPlayer.gd
@@ -2,27 +2,18 @@ extends CharacterBody2D
 
 @onready var anim_sprite = $Car
 @onready var meow_player = $MeowPlayer
-@onready var wig_node = $MikuWig
+@onready var wig_node = $BlueWig
 @onready var heart_node = $Heart
 
 func _ready() -> void:
 	set_process_input(true)
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MouseButton.LEFT:
-		if anim_sprite and _is_point_over_sprite(anim_sprite, event.position):
-			play_meow()
+
 
 func _is_point_over_sprite(sprite, global_point: Vector2) -> bool:
 	var local_point = sprite.to_local(global_point)
 	var rect = Rect2(Vector2.ZERO, sprite.frames.get_frame(sprite.animation, 0).get_size())
 	return rect.has_point(local_point)
-
-func play_meow() -> void:
-	if meow_player and not meow_player.playing:
-		meow_player.play()
-	if anim_sprite:
-		anim_sprite.play("meow_wiggle")
 
 func buy_miku_wig() -> void:
 	if wig_node:
@@ -41,3 +32,13 @@ func buy_heart() -> void:
 			heart_node.animation = anims[0]
 			heart_node.play()
 		heart_node.z_index = 10
+
+
+
+func _on_ClickArea_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		play_meow()
+
+func play_meow() -> void:
+	if not meow_player.playing:
+		meow_player.play()
