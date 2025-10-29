08852942470d0c494caaa4d7f647c7106e38e15a
Kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2025-10-29 01:23:49 +0100

    feat: 100%working health

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,16 +1,19 @@
-extends CharacterBody2D  
+extends CharacterBody2D
 
 signal died
 
-@export var medkit_drop_chance: float = 0.0002 
-@export var damage_amount: int = 1
-@export var damage_interval: float = 2.0
+@export var medkit_drop_chance: float = 0.0002
+@export var damage_amount: int = 10
+@export var damage_interval: float = 0.5
 @onready var player = get_node("/root/game/Brotat")
 
 var player_in_zone: Node = null
 var damage_timer: float = 0.0
-var health = 3  
+var health = 3
 var FT_Script = preload("res://scripts/FightingText.gd")
+const XP_SCENE = preload("res://scenes/XPpickups.tscn")
+const MEDKIT_SCENE = preload("res://scenes/MedKit.tscn")
+const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
 
 func _ready() -> void:
 	%Slime.play_walk()
@@ -40,7 +43,6 @@ func take_damage(amount: int):
 	if health <= 0:
 		drop_xp()
 		drop_medkit_random()
-		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
 		var smoke = SMOKE_SCENE.instantiate()
 		get_parent().add_child(smoke)
 		smoke.global_position = global_position
@@ -53,31 +55,28 @@ func _show_damage_popup(amount: int):
 	get_tree().current_scene.add_child(popup)
 
 func drop_xp():
-	var xp = preload("res://scenes/XPpickups.tscn").instantiate()
+	var xp = XP_SCENE.instantiate()
 	xp.global_position = global_position
 	var root = get_tree().current_scene
 	root.call_deferred("add_child", xp)
-	xp.connect("collected", root.get_node("Brotat")._on_xp_collected)
+	var player_node = get_tree().get_first_node_in_group("player")
+	if player_node:
+		xp.connect("collected", Callable(player_node, "_on_xp_collected"))
 
 func drop_medkit_random():
 	if randf() < medkit_drop_chance:
-		var medkit = preload("res://scenes/MedKit.tscn").instantiate()
+		var medkit = MEDKIT_SCENE.instantiate()
 		medkit.global_position = global_position
 		var root = get_tree().current_scene
 		root.call_deferred("add_child", medkit)
 
 func _on_KillZone_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and player_in_zone == null:
 		print("KillZone entered by:", body.name)
 		player_in_zone = body
 		damage_timer = 0.0
-
 
 func _on_KillZone_body_exited(body: Node) -> void:
 	if body == player_in_zone:
 		player_in_zone = null
 		damage_timer = 0.0
-
-func _on_DamageTimer_timeout():
-	if player_in_zone:
-		player.take_damage(damage_amount)
